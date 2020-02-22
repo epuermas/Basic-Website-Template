89b0256ebe1cc885707010d81daf9672500dbc4d
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-02-22 18:58:11 +0000

    modal working

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { Modal } from "./modal";
+import { Modal, Button } from "react-bootstrap";
 
 export const Jumbotron = () => {
+	// const { store, actions } = useContext(Context);
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 	return (
 		<div className="jumbotron jumbotron-fluid">
 			<h1 className="display-4">Eduardo Puermas</h1>
@@ -18,10 +23,34 @@ export const Jumbotron = () => {
 				leveraged towards working on projects in a team environment.
 			</p>
 			<br />
-			<button className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-				Learn more
-			</button>
+			{/* <button className="btn btn-primary btn-lg" data-toggle={Modal} data-target={Modal}>
+				Open Resume
+			</button> */}
+			<Button variant="btn btn-primary btn-lg" onClick={handleShow}>
+				Launch demo modal
+			</Button>
 			<br />
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>
+						<p>Modal heading</p>
+					</Modal.Title>
+				</Modal.Header>
+
+				<Modal.Body>
+					<p>Woohoo, youre reading this text in a modal!</p>
+				</Modal.Body>
+
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						<p>Close</p>
+					</Button>
+					<Button variant="primary" onClick={handleClose}>
+						<p>Save Changes</p>
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</div>
 	);
 };
--- src/js/component/modal.js
+++ src/js/component/modal.js
@@ -1,37 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { Button } from "react-bootstrap";
 
 export const Modal = () => {
-	return (
-		<div
-			className="modal fade"
-			id="myModal"
-			tabIndex="-1"
-			role="dialog"
-			aria-labelledby="exampleModalCenterTitle"
-			aria-hidden="true">
-			<div className="modal-dialog modal-dialog-centered" role="document">
-				<div className="modal-content">
-					<div className="modal-header">
-						<h5 className="modal-title" id="exampleModalLongTitle">
-							Modal title
-						</h5>
-						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div className="modal-body">Resume goes here!</div>
-					<div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">
-							Close
-						</button>
-						<button type="button" className="btn btn-primary">
-							Save changes
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+	return <div />;
 };
