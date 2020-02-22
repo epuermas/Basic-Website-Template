e444bd527c7e1b9146174a18bd24b49a2d6f3e04
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-02-22 21:38:29 +0000

    projects view started

--- src/js/views/projects.js
+++ src/js/views/projects.js
@@ -4,6 +4,34 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Projects = props => {
-	const { store, actions } = useContext(Context);
-	return <div className="projects-content" />;
+	return (
+		<div className="projects-content">
+			<div className="skills-header d-flex justify-content-center">
+				<h1>Projects</h1>
+			</div>
+			<p className="skills-text d-flex justify-content-center">
+				These are the projects I have worked on and completed, the list is growing and constantly being updated.
+			</p>
+			<br />
+			<div className="projects-cards d-flex justify-content-center">
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="https://i.imgur.com/5A6LNz5.jpg" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">DiscoverLaw</h5>
+						<p className="card-text">
+							Website that allows users to directly ask attorneys legal questions and locate Law Firms
+							based on legal needs.
+						</p>
+						<a
+							href="https://github.com/epuermas/Final-Project"
+							className="btn btn-primary"
+							target="_blank"
+							rel="noopener noreferrer">
+							Go to repository
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
