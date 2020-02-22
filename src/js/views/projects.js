bb586ed75c13ffdcb28a5ff49a1aad3f55cc5f71
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-02-22 22:03:28 +0000

    projects view progress 1

--- src/js/views/projects.js
+++ src/js/views/projects.js
@@ -14,7 +14,8 @@ export const Projects = props => {
 			</p>
 			<br />
 			<div className="projects-cards d-flex justify-content-center">
-				<div className="card" style={{ width: "18rem" }}>
+				{/* DiscoverLaw */}
+				<div className="discover-law card" style={{ width: "18rem" }}>
 					<img src="https://i.imgur.com/5A6LNz5.jpg" className="card-img-top" alt="..." />
 					<div className="card-body">
 						<h5 className="card-title">DiscoverLaw</h5>
@@ -24,6 +25,43 @@ export const Projects = props => {
 						</p>
 						<a
 							href="https://github.com/epuermas/Final-Project"
+							className="btn btn-primary"
+							target="_blank"
+							rel="noopener noreferrer">
+							Go to repository
+						</a>
+					</div>
+				</div>
+				{/* Contact List */}
+				<div className="contact-list card" style={{ width: "18rem" }}>
+					<img src="https://i.imgur.com/8t8YWT3.jpg" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Interactive Contact List</h5>
+						<p className="card-text">
+							Code that prints an interactive contact list allowing the user to add, edit, and delete
+							contacts using an API.
+						</p>
+						<a
+							href="https://github.com/epuermas/Functional-ContactList"
+							className="btn btn-primary"
+							target="_blank"
+							rel="noopener noreferrer">
+							Go to repository
+						</a>
+					</div>
+				</div>
+				{/* To-do List */}
+				<div className="todo-list card" style={{ width: "18rem" }}>
+					<img src="https://i.imgur.com/2XLG6v5.jpg" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Interactive To-Do List</h5>
+						<p className="card-text">
+							Code that prints an interactive to-do list allowing the user to add and delete tasks.
+							<br />
+							<br />
+						</p>
+						<a
+							href="https://github.com/epuermas/To-do-List"
 							className="btn btn-primary"
 							target="_blank"
 							rel="noopener noreferrer">
