a31b23055da7d2b4515431362c9dd33edb9b7cbf
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-12-27 17:00:34 +0000

    flux update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,7 +2,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {},
 		actions: {
-			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
