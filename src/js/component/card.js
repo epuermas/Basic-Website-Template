54951fa353ca4f4d2eccdb8880de05ba518248cf
Eduardo Puermas <eduardopuermas@hotmail.com>
2021-01-19 15:24:15 +0000

    comments added

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -8,6 +8,7 @@ export const Card = () => {
 			<img src="https://i.imgur.com/ei6DyqF.jpg" className="card-img-top" alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">Conditional Profile</h5>
+                {/* Inser your own info */}
 				<p className="card-text">Profile card that can be customized using user-input.</p>
 				<a href="#" className="btn btn-primary">
 					GitHub
